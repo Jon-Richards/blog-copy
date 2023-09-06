ecdee1d66cd05f03365ab05b5917f3d9e2468d68
Jon Richards <talljon@gmail.com>
2023-09-06 02:28:14 +0000

    docs: Add docs app.

---
+++ docs/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<html lang="en">
+  <head>
+    <title>Blog Copy</title>
+  </head>
+  <body>
+    <slot></slot>
+  </body>
+</html>
---
+++ docs/src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Blog Copy</h1>
+<p>A simple documentation app for my Blog Copy style guide.</p>
+
+<style lang="scss">
+  @use './+page.scss' as *;
+</style>
