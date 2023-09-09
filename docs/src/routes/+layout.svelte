5c2d3f0b8eda7d5fea1b8f7492511ff8be6389c4
Jon Richards <talljon@gmail.com>
2023-09-09 21:16:07 +0000

    build: Add build script and re-organize library files.

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,8 +1,79 @@
 <html lang="en">
   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Blog Copy</title>
   </head>
   <body>
-    <slot></slot>
+    <div class="main">
+      <div class="nav-column">
+        <MainNav />
+      </div>
+      <div class="content-column">
+        <slot></slot>
+      </div>
+    </div>
+    <div class="footer">
+      &copy; Jon Richards, All rights reserved.
+    </div>
   </body>
 </html>
+
+<style lang="scss">
+  @use '$lib/utils/breakpoints' as *;
+  @use '$lib/utils/colors';
+  @use '$lib/utils/dimensions';
+
+  :root {
+    --docs__footer-height: 10rem;
+
+    @include breakpoint('768') {
+      --docs__footer-height: 5rem;
+    }
+
+    @include colors.set-color-properties;
+
+    background-color: colors.get-color('shadow--dim');
+    color: colors.get-color('light--bright');
+  }
+
+  .main {
+    display: flex;
+    flex-direction: column;
+    min-height: calc(100vh - var(--docs__footer-height));
+    width: 100%;
+
+    @include breakpoint('768') {
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
+  }
+
+  .nav-column {
+    background-color: colors.get-color('shadow--mid');
+    display: none;
+
+    @include breakpoint('768') {
+      display: block;
+      flex-basis: 15rem;
+      flex-grow: 0;
+    }
+  }
+
+  .content-column {
+    flex-grow: 1;
+  }
+
+  .footer {
+    align-items: center;
+    background-color: colors.get-color('shadow--mid');
+    display: flex;
+    height: var(--docs__footer-height);
+    justify-content: center;
+    padding: 0 2rem;
+  }
+</style>
+
+<script lang="ts">
+  import MainNav from '$lib/main_nav';
+</script>
---
+++ scripts/build.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const sass = require('sass');
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
+const esbuild = require('esbuild');
+
+if (!argv.name) process.exit(1);
+
+const name = argv.name;
+const CHECKMARK = '\u2713';
+
+const sourceSASS = path.resolve(
+    __dirname, '..', 'src', 'scss', `${name}.scss`);
+const renderedSASS = sass.compile(sourceSASS);
+
+fs.writeFile(
+  path.resolve(__dirname, '..', 'dist', `${name}.css`),
+  renderedSASS.css,
+  {},
+  () => console.log(`${CHECKMARK} SASS build complete.`)
+);
+
+async function buildTS() {
+  await esbuild.build({
+    entryPoints: [
+      path.resolve(__dirname, '..', 'src', 'ts', `${name}.ts`)
+    ],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    outfile: path.resolve(__dirname, '..', 'dist', `${name}.js`)
+  });
+}
+
+buildTS()
+    .then(() => console.log(`${CHECKMARK} TS build complete.`))
+    .catch(err => console.error(err));
