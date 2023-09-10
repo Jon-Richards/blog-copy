472e8d741a30ea1591c6776e0287fceb8ed389f8
jon-richards <talljon@gmail.com>
2023-09-10 08:23:03 -0400

    scripts: Add methods to build updated CSS to docs.

--- scripts/build.js
+++ scripts/build.js
@@ -13,18 +13,40 @@ if (!argv.name) process.exit(1);
 const name = argv.name;
 const CHECKMARK = '\u2713';
 
-const sourceSASS = path.resolve(
-    __dirname, '..', 'src', 'scss', `${name}.scss`);
+const sourceSASS = path.resolve(__dirname, '..', 'src', 'scss', `${name}.scss`);
 const renderedSASS = sass.compile(sourceSASS);
 
-fs.writeFile(
+/**
+ * @param {string} outfile
+ * @param {() => void} callback
+ */
+function writeCSS(outfile, callback) {
+  fs.writeFile(
+    outfile,
+    renderedSASS.css,
+    {},
+    callback
+  );
+}
+
+writeCSS(
   path.resolve(__dirname, '..', 'dist', `${name}.css`),
-  renderedSASS.css,
-  {},
-  () => console.log(`${CHECKMARK} SASS build complete.`)
+  () => console.log(`${CHECKMARK} SASS: Build complete.`)
 );
 
-async function buildTS() {
+writeCSS(
+  path.resolve(__dirname, '..', 'docs', 'static', `${name}.css`),
+  () => console.log(`${CHECKMARK} SASS: Docs updated.`)
+);
+
+/**
+ * @param {string} outfile
+ */
+async function buildTS(outfile) {
+  if (typeof outfile !== 'string' && outfile.length < 1) {
+    throw RangeError('Invalid outfile.');
+  }
+
   await esbuild.build({
     entryPoints: [
       path.resolve(__dirname, '..', 'src', 'ts', `${name}.ts`)
@@ -33,10 +55,14 @@ async function buildTS() {
     minify: true,
     sourcemap: true,
     target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-    outfile: path.resolve(__dirname, '..', 'dist', `${name}.js`)
+    outfile
   });
 }
 
-buildTS()
-    .then(() => console.log(`${CHECKMARK} TS build complete.`))
-    .catch(err => console.error(err));
+buildTS(path.resolve(__dirname, '..', 'dist', `${name}.js`))
+  .then(() => console.log(`${CHECKMARK} TS: Build complete.`))
+  .catch(err => console.error(err));
+
+buildTS(path.resolve(__dirname, '..', 'docs', 'static', `${name}.js`))
+  .then(() => console.log(`${CHECKMARK} TS: Docs updated.`))
+  .catch(err => console.error(err));
