68f9eef4a65b414d7abc7254f7b6c5440e12bc67
Jon Richards <talljon@gmail.com>
2023-09-27 23:11:03 -0400

    blockquote: Improve markup semantics.

--- docs/src/routes/docs/(blog-copy)/html-tags/blockquote/+page.svelte
+++ docs/src/routes/docs/(blog-copy)/html-tags/blockquote/+page.svelte
@@ -1,55 +1,55 @@
 <div class="bc__block">
   <div class="bc__content bc__color-scheme--light">
     <h2>Block Quote</h2>
-    <figure class="bc__blockquote">
-      <blockquote>
+    <blockquote>
+      <div class="bc__blockquote__body">
         <p>
           When I was a child, I spoke as a child, I thought as a child:
           but when I became a man, I put away childish things.
         </p>
-      </blockquote>
+      </div>
       <footer>
         1 Corinthians 13:11
         <cite>
           King James Version
         </cite>
       </footer>
-    </figure>
+    </blockquote>
     <code class="bc__code-block">
 <pre>
-&lt;figure class="bc__blockquote"&gt;
-  &lt;blockquote&gt;
+&lt;blockquote&gt;
+  &lt;div class="bc__blockquote__body"&gt;
     &lt;p&gt;
       When I was a child, I spoke as a child, I thought as a child:
       but when I became a man, I put away childish things.
     &lt;/p&gt;
-  &lt;/blockquote&gt;
+  &lt;/div&gt;
   &lt;footer&gt;
     1 Corinthians 13:11
     &lt;cite&gt;
       King James Version
     &lt;/cite&gt;
   &lt;/footer&gt;
-&lt;/figure&gt;
+&lt;/blockquote&gt;
 </pre>
     </code>
   </div>
   <div class="bc__content bc__color-scheme--dark">
     <h3>Blockquote - Dark Mode</h3>
     <br>
-    <figure class="bc__blockquote">
-      <blockquote>
+    <blockquote>
+      <div class="bc__blockquote__body">
         <p>
           When I was a child, I spoke as a child, I thought as a child:
           but when I became a man, I put away childish things.
         </p>
-      </blockquote>
+      </div>
       <footer>
         1 Corinthians 13:11
         <cite>
           King James Version
         </cite>
       </footer>
-    </figure>
+    </blockquote>
   </div>
 </div>
